f93eea3bddfdef6c4499bfe7c8f8a537b75f9e89
Chris Cardle <cdcardle@gmail.com>
2018-01-17 19:38:36 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,14 +4,14 @@ CREATE TABLE characters (
   motto TEXT,
   species TEXT,
   author_id INTEGER,
-  sub_genre_id INTEGER
+  subgenre_id INTEGER
 )
 
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
-  sub_genre_id INTEGER
+  subgenre_id INTEGER
 )
 
 CREATE TABLE authors (
